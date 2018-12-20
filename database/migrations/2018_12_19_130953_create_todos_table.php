0b94af3981364ccbe055065484062553c244fbe7
Abbas Ali <abbas@ranium.in>
2018-12-20 16:46:00 +0530

    Created todos migration and empty controller

---
+++ database/migrations/2018_12_19_130953_create_todos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('title');
+            $table->text('description')->nullable()->default(null);
+            $table->date('due_at')->nullable()->default(null);
+            $table->datetime('completed_at')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
